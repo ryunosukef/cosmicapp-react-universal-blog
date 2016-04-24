4f4e99358bd5f78b0730ceb62d5eee6d88405d23
R.Fujimoto <fujimoto.ryunosuke@gmail.com>
2016-04-24 13:22:20 +0900

    mod link message

--- components/Pages/NoMatch.js
+++ components/Pages/NoMatch.js
@@ -26,9 +26,9 @@ export default class NoMatch extends Component {
       slug: 'home'
     })
   }
-  
+
   render(){
-    
+
     const data = this.props.data
     const page = data.page
 
@@ -39,11 +39,7 @@ export default class NoMatch extends Component {
           <div className="row">
             <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
               <div className="text-center">
-                Whoa!  Looks like you stumbled down a worm hole!<br />
-                If this is a new page that you've added in Cosmic JS, make sure you add it to your <code>routes.js</code> file!
-                <br/>
-                <br/>
-                <Link to="/">Take me home</Link>
+                <Link to="/">404 Not Found, Go HOME</Link>
               </div>
             </div>
           </div>
